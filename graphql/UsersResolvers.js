7691b50c9727572c15879ab560a443c665565dcd
Ricardo Gastón Pereyra <rgpxen@gmail.com>
2019-02-07 18:24:42 -0300

    Update UsersResolvers.js

--- graphql/UsersResolvers.js
+++ graphql/UsersResolvers.js
@@ -1,47 +1,12 @@
 // Modulos
-const Sequelize = require('sequelize');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const errors = require('./errors');
-
-// Para decidir que funcion usar para Ordenar
-function sortResults(order,model) {
-  switch(order) {
-    case 'ID_ASC': return ['id','ASC'];
-    case 'ID_DESC': return ['id','DESC'];
-    case 'CREATED_ASC' : return ['createdAt','ASC'];
-    case 'CREATED_DESC' : return ['createdAt','DESC'];
-    case 'UPDATED_ASC' : return ['updatedAt','ASC'];
-    case 'UPDATED_DESC' : return ['updatedAt','DESC'];
-    case 'USERNAME_ASC': return ['userName','ASC'];
-    case 'USERNAME_DESC': return ['userName','DESC'];
-    case 'POST_ASC': return [Sequelize.literal('PostCount'), 'ASC'];
-    case 'POST_DESC': return [Sequelize.literal('PostCount'), 'DESC'];
-    case 'TITLE_ASC': return ['title','ASC'];
-    case 'TITLE_DESC': return ['title','DESC'];
-  }
-};
-// Para crear que atributos necesitos del modelo
-const usersAtributes = (order= '') => {
-  // Que atributos necesito para devolver, en este caso no quiero devolver el password
-  const attr = [
-  'id',
-  'userName',
-  'email',
-  'role',
-  'createdAt',
-  'updatedAt',
-  ]
-  // Si busco por Post Agrego columna donde los cuenta
-  if (order === 'POST_ASC' || order === 'POST_DESC' ) 
-    attr.push([Sequelize.literal('(SELECT COUNT(*) FROM posts WHERE posts.userId = users.id)'), 'PostCount']);
-  // Devyuelvo los atributos
-  return attr;
-  };
+const {userOption} = require('./options');
 
 module.exports = {
   // ----- USER
-  // ***** Para Agregar los Posts del usuario ****
+  // ***** Para Agregar los Posts del usuario **** 
   // uso el Root (user) y la función de Sequelize que viene por asociación.
   postsUser: (user) => user.getPosts(),
   // ----- Queries
@@ -50,44 +15,21 @@ module.exports = {
   // Order el orden, default= 'ID_ASC' (ordenado por ID creciente)
   // de Context traigo los modelos de Usuaruis y Posts
   getUsers: (root,{count=-1, offset=0, order='ID_ASC', userName=''}, {users, posts}) => {
-      return users.findAll({
-        // Opciones de la busqueda
-        // Atributos que necesito
-        attributes: usersAtributes(order),
-        limit: count,
-        offset: offset,
-        // Incluyo los POSTS (join) lo necesito para buscar en la base de datos
-        include: (order === 'POST_ASC' || order === 'POST_DESC' ) ? [posts] : [],
-        where: {userName: { $like: `%${userName}%`}},
-        // Como los ordeno
-        order: [
-          sortResults(order)
-        ]
-      // Devuelvo el resultado
-      }).then( usersList => usersList); 
+      return users.findAll(userOption(userName, count, offset, order, posts))
+        .then( usersList => usersList); 
     },
   // **** Busca UN usuario por ID ****
   getUser: (root, {id}, {users, posts}) => {
-      return users.findOne({
-        // Opciones de la busqueda
-        attributes: usersAtributes(),
-        where: {id: id},
-        //include: [posts]
-      // Devuelvo el resultado
-      }).then( user => user);
+      return users.findOne({ where: {id: id} })
+        .then( user => user);
     },
   // **** Busca el Usuario Loggeado ****
   me: (root, args, {auth, users, posts}) => {
     // Si no esta loggeado ERROR
       if (!auth) throw new Error(errors.LOG_01);
     // Sino
-      return users.findOne({
-        // Opciones de la busqueda
-        attributes: usersAtributes(),
-        where: {id: auth},
-        //include: [posts]
-        // Devuelvo el resultado
-        }).then( user => user);
+      return users.findOne({ where: {id: auth} })
+        .then( user => user);
     },
   // **** Chequea si el Nombre de Usuario esta Disponible ****
   isUserName: (root, {userName}, { users }) => {
@@ -140,14 +82,14 @@ module.exports = {
       // Si el email o el usuario existen, o password de longitud erronea, o email no es email (u otro error) 
         .catch(err => new Error(errors.SIGN_01));
     },
-  // **** Actualiza un user ****
+  // **** Actualiza un User****
   updateUser: (root, {input}, { auth, users })=> {
       if (!auth) throw new Error(errors.LOG_01);
       return users.update(input,{where:{ id: auth }})
                   .then(user => users.findOne({where:{id: auth}}))
                   .catch(err => "Error: "+err);
     },
-  // **** Cambia el Rol de un User ****
+  // **** Cambia de Rol de un User ****
   changeRole: (root, {userId, role}, { auth, users })=> {
       if (!auth) throw new Error(errors.LOG_01);
       return users.findOne({where:{id: auth}})
