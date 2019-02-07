e5cfb526760729ac82fa3cba3bf31f797c983d47
Ricardo Gastón Pereyra <rgpxen@gmail.com>
2019-02-06 23:20:07 -0300

    Create errors.js

---
+++ graphql/errors.js
@@ -0,0 +1,6 @@
+module.export = {
+  LOG_01: "Debe Estar Loggeado Para Continuar",
+  LOG_02: "Ya esta Loggeado.",
+  LOG_03: "Usuario o Contraseña Incorrectos",
+  SIGN_01: "No se pudo Crear Usuario",
+}
