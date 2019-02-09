0b272b11538f063952ff944d203fd401b324ded2
Ricardo Gastón Pereyra <rgpxen@gmail.com>
2019-02-08 23:49:55 -0300

    Update errors.js

--- graphql/errors.js
+++ graphql/errors.js
@@ -1,6 +1,12 @@
-module.export = {
+module.exports = {
   LOG_01: "Debe Estar Loggeado Para Continuar",
   LOG_02: "Ya esta Loggeado.",
   LOG_03: "Usuario o Contraseña Incorrectos",
-  SIGN_01: "No se pudo Crear Usuario",
+  LOG_04: "Datos del Usuario Incorrecto",
+  LOG_05: "Deber ser ADMIN",
+  SIGN_01: "No se pudo Crear Usuario, Usuario o Email existentes",
+  SIGN_02: "Contraseña muy corta",
+  SEARCH_00: "No se pudo buscar",
+  SEARCH_01: "No hay resultados",
+  
 }
