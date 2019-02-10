a818a03ca032a374e7fca2b67dd984c1afdf42a3
Ricardo Gastón Pereyra <rgpxen@gmail.com>
2019-02-09 22:42:44 -0300

    Update errors.js

--- graphql/errors.js
+++ graphql/errors.js
@@ -6,7 +6,13 @@ module.exports = {
   LOG_05: "Deber ser ADMIN",
   SIGN_01: "No se pudo Crear Usuario, Usuario o Email existentes",
   SIGN_02: "Contraseña muy corta",
+  CREATE_00: "No se pudo Crear",
+  UPDATE_00: "No se puede Actualizar.",
+  UPDATE_01: "Usuario Incorrecto.",
+  UPDATE_01: "No se puede cambiar a ADMIN.",
   SEARCH_00: "No se pudo buscar",
   SEARCH_01: "No hay resultados",
+  DELETE_00: "No se pudo Borrar",
+  NO_EMPTY: "Tiene que estar Vacio"
   
 }
