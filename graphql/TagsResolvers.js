9c35a0e8b83e9974a6b7e0f066c0126703f21410
Ricardo Gastón Pereyra <rgpxen@gmail.com>
2019-02-09 22:42:19 -0300

    Update TagsResolvers.js

--- graphql/TagsResolvers.js
+++ graphql/TagsResolvers.js
@@ -15,10 +15,30 @@ module.exports = {
   getTags: (root, {count=-1, offset=0, order='ID_ASC', name=''}, {tags, posts}) => {
       return tags.findAll(tagOption(name,count,offset,order,posts))
         .then( tagList => tagList)
-    },
+        .catch(err => new Error(errors.SEARCH_00));
+  },
   // ***** Encuentra un Tag
   getTag: (root, {id}, {auth, tags}) => {
       return tags.find({ where: {id: id} })
         .then( tag => tag)
-    },
+        .catch(err => new Error(errors.SEARCH_01));
+  },
+  // **** Borra un User ****
+  deleteTag: (root, {id}, { auth, tags })=> {
+    // Debe Estar Loggeado
+    if (!auth) throw new Error(errors.LOG_01);
+    // Debe ser Admin
+    if (auth.role <2) throw new Error(errors.LOG_05);
+    // Busco el user a borrar
+    return tags.findOne({ where: { id } })
+      .then(tag => {
+        if (tag.getPosts().length >0)
+          throw new Error(errors.NO_EMPTY);
+        // Si lo encuentro lo borro
+        tag.destroy();
+        // devuelvo el borrado
+        return tag;
+      // Sino tira error
+      }).catch(err => new Error(errors.DELETE_00+" "+err));
+  },
 }
