1e23f1101f07e501fc8c662e7c89382cc2d86ccd
Ricardo Gastón Pereyra <rgpxen@gmail.com>
2019-02-10 22:57:25 -0300

    Update errors.js

--- graphql/errors.js
+++ graphql/errors.js
@@ -9,9 +9,10 @@ module.exports = {
   CREATE_00: "No se pudo Crear",
   UPDATE_00: "No se puede Actualizar.",
   UPDATE_01: "Usuario Incorrecto.",
-  UPDATE_01: "No se puede cambiar a ADMIN.",
+  UPDATE_02: "No se puede cambiar a ADMIN.",
   SEARCH_00: "No se pudo buscar",
   SEARCH_01: "No hay resultados",
+  SEARCH_02: "No existe",
   DELETE_00: "No se pudo Borrar",
   NO_EMPTY: "Tiene que estar Vacio"
   
