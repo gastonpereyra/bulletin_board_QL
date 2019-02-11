9d6bc13ff27027ee07fd19e259ad4b548ac96548
Ricardo Gastón Pereyra <rgpxen@gmail.com>
2019-02-10 22:59:13 -0300

    Update users.js

--- models/users.js
+++ models/users.js
@@ -6,7 +6,9 @@ const model = {
   userName: { type: Sequelize.STRING, unique: true, allowNull: false },
   email: { type: Sequelize.STRING, unique: true, allowNull: false, validate: { isEmail: true } },
   role: { type: Sequelize.INTEGER, allowNull: false, defaultValue: 0, validate: { min: 0, max: 2 } },
-  password: { type: Sequelize.STRING, allowNull: false }
+  password: { type: Sequelize.STRING, allowNull: false },
+  image: { type: Sequelize.STRING, allowNull: true, validate: {isUrl: true}},
+  lastLoginAt: { type: Sequelize.DATE }
 };
 // Hooks
 const hooks = {
