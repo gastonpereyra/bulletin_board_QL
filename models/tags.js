b76e318b0e18f2643e9c053f6ac6bb762c080bda
Ricardo Gastón Pereyra <rgpxen@gmail.com>
2019-02-10 22:59:03 -0300

    Update tags.js

--- models/tags.js
+++ models/tags.js
@@ -2,7 +2,7 @@
 const Sequelize = require('sequelize');
 // Modelo para Los Tags
 const model = {
-  name: { type: Sequelize.STRING, unique: true, allowNull: false },
+  name: { type: Sequelize.STRING, unique: true, allowNull: false, len:[1,20] },
 };
 
 // Exportar
