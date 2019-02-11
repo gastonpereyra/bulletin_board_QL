bde69b5ff60028674781e09727307ce2627ac584
Ricardo Gastón Pereyra <rgpxen@gmail.com>
2019-02-10 22:56:12 -0300

    Update PostsResolvers.js

--- graphql/PostsResolvers.js
+++ graphql/PostsResolvers.js
@@ -2,6 +2,15 @@
 const Sequelize = require('sequelize');
 const errors = require('./errors');
 
+const addTagging = (tagList, post, tags) => {
+  tagList.forEach( name => {
+    tags.findOne({where: {name}}).then( 
+      tag => tag ? post.addTags(tag.id) : 
+      tags.create({ name }).then( ntag => post.addTags(ntag.id))
+    );
+  });
+};
+
 module.exports = {
   // ----- POST
   // ***** Agregar el User a Post
@@ -12,122 +21,102 @@ module.exports = {
   // ***** Agregar los Comentarios a Post
   commentsPost: (post) => post.getComments(),
   // ***** Agregar los Like/Dislikes a Post
-  likesPost: (post) => post.getLikes().then( like => like.reduce( (total,val) => total += val.like === 'l' ? 1 : 0, 0 )),
-  dislikesPost: (post) => post.getLikes().then( like => like.reduce( (total,val) => total += val.like === 'd' ? 1 : 0, 0)),
+  likesPost: (post) => post.getLikes().then( likeList => likeList.reduce( (total, actual) => total+ actual ? 1 : 0, 0)),
+  dislikesPost: (post) => post.getLikes().then( likeList => likeList.reduce( (total, actual) => total+ !actual ? 1 : 0, 0)),
   // ----- QUERY
-  getPosts: (root, {count=-1, offset=0}, {auth, users, posts}) => {
+  getPosts: (root, {title='', minLikes=0, minDislikes=0, count=-1, offset=0, order='ID_ASC' }, {auth, users, posts, likes}) => {
       return posts.findAll({
         limit: count,
         offset: offset,
-        order: [ ['id', "DESC"] ]
+        // order: [ [likes, "likeCount"] ]
       }).then( results => results.map( post => {
-        /*
-        post.author= post.getUser();
-        post.tags= post.getTags();
-        post.comments= post.getComments();
-        post.likes = post.getLikes().then( like => like.reduce( (total,val) => total += val.like === 'l' ? 1 : 0, 0 ));
-        post.dislikes = post.getLikes().then( like => like.reduce( (total,val) => total += val.like === 'd' ? 1 : 0, 0));*/
         return post;
         })
       )
     },
     getPost: (root, {id}, {auth, users, posts}) => {
-      return posts.findOne({where: {id: id}}).then( post => {
-        /*
-        post.author= post.getUser();
-        post.tags= post.getTags();
-        post.comments= post.getComments();
-        post.likes = post.getLikes().then( like => like.reduce( (total,val) => total += val.like === 'l' ? 1 : 0, 0 ));
-        post.dislikes = post.getLikes().then( like => like.reduce( (total,val) => total += val.like === 'd' ? 1 : 0, 0));*/
-        return post;
-        })
+      return posts.findOne({where: {id: id}})
+        .then( post => post)
+        .catch(err => new Error(errors.SEARCH_00));
     },
     getPostByTitle: (root, {title}, {auth, users, posts}) => {
       return posts.findAll({where: {title: {$like: '%'+title+'%'}}}).then( results => results.map( post => {
-        /*
-        post.author= post.getUser();
-        post.tags= post.getTags();
-        post.comments= post.getComments();
-        post.likes = post.getLikes().then( like => like.reduce( (total,val) => total += val.like === 'l' ? 1 : 0, 0 ));
-        post.dislikes = post.getLikes().then( like => like.reduce( (total,val) => total += val.like === 'd' ? 1 : 0, 0));
-        */
         return post;
         })
       )
     },
   // ----- MUTATIONS
-  createPost: (root,{title,message,tagList},{auth, users, posts, tags}) => {
-      if (!auth) throw new Error("Debe estar Loggeado");
-      return posts.create({title, message, userId: auth}).then( post => {
-        const tagging= tagList.length === 0 ? null : tagList.map(tag => {
-          return tags.findOrCreate({where: {name: tag}})
-              .spread( (result,created) => {
-                post.addTags(result.get({plain: true}).id);
-                return result.get({plain: true})
-          });
+  createPost: (root,{title,message,tagList=[]},{auth, posts, tags}) => {
+    // Si no esta loggeado no se puede hacer esta acción
+    if (!auth) throw new Error(errors.LOG_01);
+    return posts.create({title, message, userId: auth.id})
+      .then( post => {
+        addTagging(tagList, post, tags);
+        return post
+    }).catch(err => new Error(errors.CREATE_00));
+  },
+  updatePost: (root, {id,title,message,tagList}, { auth, posts,tags })=> {
+    // Si no esta loggeado no se puede hacer esta acción
+    if (!auth) throw new Error(errors.LOG_01);
+    return posts.findOne({where: {id: id}}).then( post => {
+      if (auth.role > 0 || post.userId === auth.id) { 
+        
+        post.getTags()
+          .then( oldTags => {
+            oldTags.forEach( oldTag => {
+              if (!tagList.includes(oldTag.name))
+                post.removeTags(oldTag);
+            });
+            return oldTags;
+          }).then( oldTags => {
+            const newTags = tagList.map( newTag => {
+              if (!oldTags.includes(newTag.name))
+                return newTag;
+            });
+            addTagging(newTags, post, tags);
         });
-        post.author= post.getUser();
-        post.tags= tagging;
-        post.likes= 0;
-        post.dislikes= 0;
-        return post;
-      });
-    },
-    updatePost: (root, {id,title,message,tagList}, { auth, posts,tags })=> {
-      if (!auth) throw new Error("Debe estar Loggeado");
-      return posts.findOne({where: {id: id}}).then( post => {
-        if (post.userId !== auth) 
-          throw new Error("Usuario Incorrecto");
-        posts.update({title, message},{where:{ id: id, userId: auth }});
-        post.getTags().then( oldTags => {
-          oldTags.forEach( oldTag => {
-          if (!tagList.includes(oldTag.name)){
-            post.remove(oldTag);
-            }
-          });
-          const tagging= tagList.map( newTag => {
-              tags.findOrCreate({where: {name: newTag}})
-                .spread( (result,created) => {
-                  return post.addTags(result.get({plain: true}).id);
+        return post.update({title, message}).catch(err => new Error(errors.UPDATE_00));
+      }
+      throw new Error(errors.UPDATE_01);
+      })
+  },
+  deletePost: (root, {id}, { auth, comments, posts })=> {
+      // Si no esta loggeado no se puede hacer esta acción
+    if (!auth) throw new Error(errors.LOG_01);
+    return posts.findOne({where:{id: id}})
+      .then( post => {
+        if (!post) throw new Error(errors.SEARCH_02);
+        if (auth.role > 0 || post.userId === auth.id) { 
+          post.getTags()
+          .then( oldTags => {
+            oldTags.forEach( oldTag => {
+              post.removeTags(oldTag);
             });
           });
-        });
-        
-        post.tags= post.getTags();
-        post.author= post.getUser();
-        post.comments= post.getComments();
-        post.likes = post.getLikes().then( like => like.reduce( (total,val) => total += val.like === 'l' ? 1 : 0, 0 ));
-        post.dislikes = post.getLikes().then( like => like.reduce( (total,val) => total += val.like === 'd' ? 1 : 0, 0));
-        return post;
-        })
-    },
-    deletePost: (root, {id}, { auth, users, posts })=> {
-      if (!auth) throw new Error("Debe estar Loggeado");
-      return users.findOne({where:{id: auth}})
-        .then( user => {
-          if (user.role <2) throw new Error("Debe ser Admin")
-          return posts.findOne({where:{id: id}})
-                  .then(post => {
-                    posts.destroy({where:{id: id}});
-                    post.author= post.getUser();
-                    return post;
-                  })
-    })},
+          comments.destroy({where: {postId: id}});
+          post.destroy();
+          return post;
+        }
+        throw new Error(errors.UPDATE_01);
+    });  
+  },
   
   giveLike: (root,{postId},{auth, likes}) => {
-      if (!auth) throw new Error("Debe estar Loggeado");
-      return likes.find({where:{ postId: postId, userId: auth}})
+      // Si no esta loggeado no se puede hacer esta acción
+    if (!auth) throw new Error(errors.LOG_01);
+      return likes.find({where:{ postId: postId, userId: auth.id}})
         .then(isLike => {
-        if (isLike) return likes.destroy({where:{ postId: postId, userId: auth}}).then(() => false);
-        return likes.create({postId, like: 'l', userId: auth}).then( () => true)
+        if (isLike) return isLike.destroy().then(() => false);
+        return likes.create({postId, like: true, userId: auth.id}).then( () => true)
       });
     },
     giveDislike: (root,{postId},{auth, likes}) => {
-      if (!auth) throw new Error("Debe estar Loggeado");
-      return likes.find({where:{ postId: postId, userId: auth}})
+      // Si no esta loggeado no se puede hacer esta acción
+    if (!auth) throw new Error(errors.LOG_01);
+      return likes.find({where:{ postId: postId, userId: auth.id}})
         .then(isDislike => {
-        if (isDislike) return likes.destroy({where:{ postId: postId, userId: auth}}).then(() => false);;
-        return likes.create({postId, like: 'd', userId: auth}).then( () => true)
+        if (isDislike) return isDislike.destroy().then(() => false);;
+        return likes.create({postId, like: false, userId: auth.id}).then( () => true)
       });
     }
 }
